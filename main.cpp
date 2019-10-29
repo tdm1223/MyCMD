53cbd1e5a02430397f3509832c5391f802cb7f01
tdm1223 <tdm1223@naver.com>
2019-10-29 18:33:14 +0900

    [UPDATE] add start, echo command
    두 명령어를 같이 쓸때 나타나는 버그 수정 필요

--- main.cpp
+++ main.cpp
@@ -9,21 +9,41 @@
 #define STR_LEN    256
 #define CMD_TOKEN_NUM  10
 
-TCHAR ERROR_CMD[]
-= _T("'%s'은(는) 실행할 수 있는 프로그램이 아닙니다. \n");
+TCHAR cmdString[STR_LEN];
+TCHAR cmdTokenList[CMD_TOKEN_NUM][STR_LEN];
+TCHAR seps[] = _T(" ,\t\n");
 
-int CmdProcessing(void);
+TCHAR ERROR_CMD[] = _T("'%s'은(는) 실행할 수 있는 프로그램이 아닙니다. \n");
+
+int CmdProcessing(int tokenNum);
 TCHAR * StrLower(TCHAR *);
+int CmdReadTokenize();
 
-int main(int agrc, TCHAR * argv[])
+int main(int argc, TCHAR * argv[])
 {
 	// 한글 입력을 가능케 하기 위해.
 	_tsetlocale(LC_ALL, _T("Korean"));
 
-	DWORD isExit;
+	if (argc > 2)
+	{
+		for (int i = 1; i < argc; i++)
+		{
+			_tcscpy(cmdTokenList[i - 1], argv[i]);
+		}
+		CmdProcessing(argc - 1);
+	}
+	DWORD isExit = NULL;
 	while (1)
 	{
-		isExit = CmdProcessing();
+		int tokenNum = CmdReadTokenize();
+
+		// 명령의 미입력
+		if (tokenNum == 0)
+		{
+			continue;
+		}
+
+		isExit = CmdProcessing(tokenNum);
 		if (isExit == TRUE)
 		{
 			_fputts(_T("명령어 처리를 종료합니다. \n"), stdout);
@@ -34,44 +54,87 @@ int main(int agrc, TCHAR * argv[])
 	return 0;
 }
 
-TCHAR cmdString[STR_LEN];
-TCHAR cmdTokenList[CMD_TOKEN_NUM][STR_LEN];
-TCHAR seps[] = _T(" ,\t\n");
-
-int CmdProcessing(void)
+// 명령을 입력받아 토큰화하는 함수
+// 토큰 개수 반환
+int CmdReadTokenize()
 {
-	_fputts(_T("Best command prompt>> "), stdout);
+	TCHAR * token;
+
+	_fputts(_T("Command Prompt>> "), stdout);
 	_getts_s(cmdString, sizeof(cmdString));
 
-	TCHAR * token = _tcstok(cmdString, seps);
-
+	token = _tcstok(cmdString, seps);
 	int tokenNum = 0;
 	while (token != NULL)
 	{
-		_tcscpy(
-			cmdTokenList[tokenNum++], StrLower(token)
-		);
+		// 토큰 리스트에 대소문자 구별없이 차례대로 저장
+		_tcscpy(cmdTokenList[tokenNum++], StrLower(token));
 		token = _tcstok(NULL, seps);
 	}
+
+	return tokenNum;
+}
+
+int CmdProcessing(int tokenNum)
+{
+	BOOL isRun;
+	STARTUPINFO si = { 0, };
+	PROCESS_INFORMATION pi;
+
+	TCHAR cmdStringWithOptions[STR_LEN] = { 0, };
+	TCHAR optString[STR_LEN] = { 0, };
+
+	si.cb = sizeof(si);
 
 	if (!_tcscmp(cmdTokenList[0], _T("exit")))
 	{
 		return TRUE;
 	}
-	else if (!_tcscmp(cmdTokenList[0], _T("추가 되는 명령어 1")))
+	else if (!_tcscmp(cmdTokenList[0], _T("start")))
 	{
+		// start를 제외한 나머지 문자열을 재구성
+		if (tokenNum > 1)
+		{
+			for (int i = 1; i < tokenNum; i++)
+			{
+				_stprintf(optString, _T("%s %s"), optString, cmdTokenList[i]);
+				_stprintf(cmdStringWithOptions, _T("%s %s"), _T("windows.exe"), optString);
+			}
+		}
+		// start만 입력
+		else
+		{
+			_stprintf(cmdStringWithOptions, _T("%s"), _T("windows.exe"));
+		}
 
+		isRun = CreateProcess(NULL, cmdStringWithOptions, NULL, NULL, TRUE, CREATE_NEW_CONSOLE, NULL, NULL, &si, &pi);
+		CloseHandle(pi.hProcess);
+		CloseHandle(pi.hThread);
 	}
-	else if (!_tcscmp(cmdTokenList[0], _T("추가 되는 명령어 2")))
+	else if (!_tcscmp(cmdTokenList[0], _T("echo")))
 	{
+		for (int i = 1; i < tokenNum; i++)
+		{
+			_stprintf(optString, _T("%s %s"), optString, cmdTokenList[i]);
+
+		}
+		_tprintf(_T("echo message : %s \n"), optString);
 	}
 	else
 	{
 		// 프로그램 실행
-		STARTUPINFO si = { 0, };
-		PROCESS_INFORMATION pi;
-		si.cb = sizeof(si);
-		BOOL isRun = CreateProcess(NULL, cmdTokenList[0], NULL, NULL, TRUE, 0, NULL, NULL, &si, &pi);
+		_tcscpy(cmdStringWithOptions, cmdTokenList[0]);
+
+		for (int i = 1; i < tokenNum; i++)
+		{
+			_stprintf(cmdStringWithOptions, _T("%s %s"), cmdStringWithOptions, cmdTokenList[i]);
+		}
+
+		isRun = CreateProcess(NULL, cmdStringWithOptions, NULL, NULL, TRUE, 0, NULL, NULL, &si, &pi);
+
+		CloseHandle(pi.hProcess);
+		CloseHandle(pi.hThread);
+
 		if (isRun == FALSE)
 		{
 			_tprintf(ERROR_CMD, cmdTokenList[0]);
